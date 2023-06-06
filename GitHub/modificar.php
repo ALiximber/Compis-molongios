901995977a7855a0a68813f9cca9460098495213
ALiximber <99149881+ALiximber@users.noreply.github.com>
2023-06-06 11:48:53 -0600

    xvcbncvn

--- GitHub/modificar.php
+++ GitHub/modificar.php
@@ -1,36 +1,33 @@
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    
-    <title>Conexión BD</title>
-</head>
+<html>
+    <head>
+        <title>Conexion BD</title>
+    </head>
     <body>
         <center>
             <?php
                 $id=$_REQUEST['id'];
                 include("conexion.php");
-                $query="SELECT * FROM usuarios WHERE id='$id'";
-                $resultado = $conexion->query($query);
+                $query="SELECT * FROM progsalud where id='$id'";
+                $resultado=$conexion->query($query);
                 $row=$resultado->fetch_assoc();
-                ?>
-        <form method="post" action="modificar_proceso.php?id=<?php echo $row['id'];?>">
-
-                Ingrese el nombre:
-                <input type="text" REQUIRED name="nombre" placeholder="Nombre" value="<?php echo $row['nombre']; ?>"/>
-                <br>
-                <br>
-                Ingrese el apellido:
-                <input type="text" REQUIRED name="apellidos" placeholder="apellidos" value="<?php echo $row['apellidos']; ?>"/>
-                <br>
-                <br>
-                <br>
-                Ingrese el correo:
-                <input type="text" REQUIRED name="correo" placeholder="correo" value="<?php echo $row['correo']; ?>"/>
-                <br>
-                <br>
-                <br>
-                <input type="submit" value="Aceptar">
-            </center>
-        </form> 
+            ?>
+    <form method="post" action="modificar_proceso.php?id=<?php echo $row['id'];?>">
+                Ingrese el nombre completo del paciente:
+                <input type="text" required name="Nombre" placeholder="Nombre"><br><br>
+                Ingrese la Edad:
+                <input type="text" required name="Edad" placeholder="Edad"><br><br>
+                Ingrese el sexo:
+                <input type="text" required name="Sexo" placeholder="Sexo"><br><br>
+                Ingrese el tipo de sangre:
+                <input type="text" required name="Sangre" placeholder="Tipo de sangre"><br><br>
+                Ingrese el número de habitación:
+                <input type="text" required name="Habitacion" placeholder="Num. Habitación"><br><br>
+                Ingrese el diagnóstico:
+                <input type="text" required name="Diag" placeholder="Diagnóstico"><br><br>
+                Ingrese la CURP:
+                <input type="text" required name="Curp" placeholder="CURP"><br><br>
+        <input type="submit" value="Aceptar">
+        </center>
+    </form>
     </body>
 </html>
--- GitHub/modificar_proceso.php
+++ GitHub/modificar_proceso.php
@@ -7,11 +7,11 @@
     $Sexo = $_POST['Sexo'];
     $Sangre = $_POST['Sangre'];
     $Hab = $_POST['Habitacion'];
-    $diag = $_POST['Diagnostico'];
-    $CURP = $_POST['CURP'];
+    $diag = $_POST['Diag'];
+    $CURP = $_POST['Curp'];
     
     
-    $query = "UPDATE ProgSalud set nombre= '$nom' , apellidos='$ape', correo= '$cor'  WHERE id='$id'";
+    $query = "UPDATE progsalud set Nombre= '$nom' , Edad='$Edad', Sexo= '$Sexo', Sangre='$Sangre', Habitacion='$Hab', Diag='$diag', Curp='$CURP' WHERE id='$id'";
     $resultado=$conexion->query($query);
 
     if($resultado){
