1e4b01d87f214b112b4bdeca82936a0922337354
ALiximber <99149881+ALiximber@users.noreply.github.com>
2023-06-09 11:29:52 -0600

    fdxgh

--- GitHub/modificar.php
+++ GitHub/modificar.php
@@ -1,33 +1,57 @@
-<html>
-    <head>
-        <title>Conexion BD</title>
-    </head>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    
+    <title>Conexión BD</title>
+</head>
     <body>
         <center>
             <?php
                 $id=$_REQUEST['id'];
                 include("conexion.php");
-                $query="SELECT * FROM progsalud where id='$id'";
-                $resultado=$conexion->query($query);
+                $query="SELECT * FROM progsalud WHERE id='$id'";
+                $resultado = $conexion->query($query);
                 $row=$resultado->fetch_assoc();
-            ?>
-    <form method="post" action="modificar_proceso.php?id=<?php echo $row['id'];?>">
-                Ingrese el nombre completo del paciente:
-                <input type="text" required name="Nombre" placeholder="Nombre"><br><br>
-                Ingrese la Edad:
-                <input type="text" required name="Edad" placeholder="Edad"><br><br>
-                Ingrese el sexo:
-                <input type="text" required name="Sexo" placeholder="Sexo"><br><br>
+                ?>
+        <form method="post" action="modificar_proceso.php?id=<?php echo $row['id'];?>">
+
+                Ingrese el nombre completo:
+                <input type="text" REQUIRED name="Nombre" placeholder="Nombre" value="<?php echo $row['Nombre']; ?>"/>
+                <br>
+                <br>
+                Ingrese el Edad:
+                <input type="text" REQUIRED name="Edad" placeholder="apellidos" value="<?php echo $row['Edad']; ?>"/>
+                <br>
+                <br>
+                <br>
+                Ingrese el Sexo:
+                <input type="text" REQUIRED name="Sexo" placeholder="correo" value="<?php echo $row['Sexo']; ?>"/>
+                <br>
+                <br>
+                <br>
+
                 Ingrese el tipo de sangre:
-                <input type="text" required name="Sangre" placeholder="Tipo de sangre"><br><br>
-                Ingrese el número de habitación:
-                <input type="text" required name="Habitacion" placeholder="Num. Habitación"><br><br>
-                Ingrese el diagnóstico:
-                <input type="text" required name="Diag" placeholder="Diagnóstico"><br><br>
+                <input type="text" REQUIRED name="Sangre" placeholder="apellidos" value="<?php echo $row['Sangre']; ?>"/>
+                <br>
+                <br>
+                <br>
+                Ingrese la habitación:
+                <input type="text" REQUIRED name="Habitacion" placeholder="apellidos" value="<?php echo $row['Habitacion']; ?>"/>
+                <br>
+                <br>
+                <br>
+                Ingrese el diagnostico:
+                <input type="text" REQUIRED name="Diag" placeholder="apellidos" value="<?php echo $row['Diag']; ?>"/>
+                <br>
+                <br>
+                <br>
                 Ingrese la CURP:
-                <input type="text" required name="Curp" placeholder="CURP"><br><br>
-        <input type="submit" value="Aceptar">
-        </center>
-    </form>
+                <input type="text" REQUIRED name="Curp" placeholder="apellidos" value="<?php echo $row['Curp']; ?>"/>
+                <br>
+                <br>
+                <br>
+                <input type="submit" value="Aceptar">
+            </center>
+        </form> 
     </body>
 </html>
